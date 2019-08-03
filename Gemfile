927eac701f8fcc12252932f697f7afb7a4b571f4
Usamah Jamaludin <usamah82@gmail.com>
2019-08-03 23:45:12 +0800

    Annotate.

--- Gemfile
+++ Gemfile
@@ -59,6 +59,7 @@ group :test do
 end
 
 group :development do
+  gem "annotate"
   gem "graphiql-rails"
   gem "listen", ">= 3.0.5", "< 3.2"
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  first_name             :string           default(""), not null
+#  last_name              :string           default(""), not null
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  jti                    :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  failed_attempts        :integer          default(0), not null
+#  unlock_token           :string
+#  locked_at              :datetime
+#  role                   :integer
+#
+
 require "rails_helper"
 
 RSpec.describe User, type: :model do
